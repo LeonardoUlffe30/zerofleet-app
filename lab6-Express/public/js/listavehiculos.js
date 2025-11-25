44def73c182f179ac4f16bc53ae875ebc0208a02
UCM-AliC <alichen@ucm.es>
2025-11-25 11:05:29 +0100

    Pto 1 y 2 realizado

---
+++ lab6-Express/public/js/listavehiculos.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", cargarVehiculos);
+
+function cargarVehiculos() {
+    fetch('/vehiculos/api/vehiculos')
+        .then(response => response.json())
+        .then(vehiculos => {
+            const tbody = document.querySelector('#tablavehiculos tbody');
+            tbody.innerHTML = '';
+            vehiculos.forEach(v => {
+                const accciones = (usuario && usuario.tipo === "admin") ? `
+                    <td class = "fit">
+                        <a href ="/vehiculos/${v.id}/editar" class="btn btn-light">Editar</a>
+                        <a href ="/vehiculos/${v.id}/eliminar" class="btn btn-danger"> Eliminar</a>
+                    </td>`: '';
+                const fila =`
+                <tr>
+                  <td><img src="/img/imgVehiculos/${v.imagen}" alt="Imagen del vehiculo" width="100"></td>
+                  <td>${v.id}</td>
+                  <td>${v.marca}</td>
+                  <td>${v.autonomia}</td>
+                  <td>${v.tipo}</td>
+                  <td>${v.modelo}</td>
+                  ${accciones}
+                </tr>`;
+
+                tbody.innerHTML += fila;
+            });
+        }).catch(error => console.error("Error al cargar los vehiculos:", error));
+}
