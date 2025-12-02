18a645c30048d44a30a8f975d0b808a4219abdc5
Leonardo <leonardo.ulffe.2003@gmail.com>
2025-12-02 04:10:50 +0100

    funcion crear reserva del controller lista (crea cliente y reserva)

--- lab6-Express/controllers/reservasController.js
+++ lab6-Express/controllers/reservasController.js
@@ -13,11 +13,11 @@ function query(sql, params = []) {
 // LISTAR RESERVAS
 
 function listarReservas(request, response) {
-    response.status(200).render("listareservas", { 
+    response.status(200).render("listareservas", {
         titulo: "Lista de reservas",
         estilo: "listavehiculos.css",
         script: "",
-     });
+    });
 }
 
 async function listarReservasApi(request, response) {
@@ -29,12 +29,26 @@ async function listarReservasApi(request, response) {
 
 // CREAR RESERVAS
 
-function formulariocrearReserva(request, response) {
-    response.status(200).render("reservas", {
-        titulo: "Reservas",
-        estilo: "reservas.css",
-        script: "reservas.js"
-    });
+async function formulariocrearReserva(request, response) {
+    try {
+        const idConcesionario = request.session.usuario.id_concesionario;
+
+        const sql = `SELECT matricula FROM vehiculos WHERE id_concesionario = ? AND activo = true`;
+        const params = [idConcesionario];
+
+        const vehiculos = await query(sql, params);
+
+        response.status(200).render("reservas", {
+            titulo: "Reservas",
+            estilo: "reservas.css",
+            script: "reservas.js",
+            vehiculos: vehiculos
+        });
+
+    } catch (error) {
+        console.error("Error cargando vehículos:", err.message);
+        response.status(500).send("Error interno cargando vehículos");
+    }
 }
 
 async function crearReserva(request, response) {
@@ -43,42 +57,52 @@ async function crearReserva(request, response) {
         const err = validationResult(request);
         if (!err.isEmpty()) {
             console.log("Errores de validación:", err.array());
-            return response.status(400).json({ errores: err.array()})
+            return response.status(400).json({ errores: err.array() })
         }
 
-        const { nombre, apellido, correo, telefono, tipo, fechaIni, horaIni, fechaFin, horaFin, duracion } = request.body;
+        const { nombreCliente, apellidoCliente, correoCliente, telefonoCliente, vehiculo, fechaIni, horaIni, fechaFin, horaFin, duracion } = request.body;
 
-        //Se comprueba si el vehiculo esta disponible
-        let sql = `SELECT * FROM vehiculos WHERE matricula = ? AND estado != 'disponible'`;
-        let params = [tipo];
-        let vehiculo = await query(sql, params);
-        if(vehiculo.length > 0) {
-            return response.status(401).json({ mensaje: "Vehiculo no disponible" });
+        console.log(nombreCliente, apellidoCliente, correoCliente, telefonoCliente, vehiculo, fechaIni, horaIni, fechaFin, horaFin, duracion);
+
+        // Verificar si existe cliente
+        let sql = `SELECT * FROM clientes WHERE correo = ?`;
+        let params = [correoCliente];
+
+        let cliente = await query(sql, params);
+        let id_cliente;
+
+        if (cliente.length === 0) {
+            // Insertar cliente
+            sql = `INSERT INTO clientes (nombre, apellido, correo, telefono)
+                   VALUES (?, ?, ?, ?)`
+            params = [nombreCliente, apellidoCliente, correoCliente, telefonoCliente];
+
+            cliente = await query(sql, params);
+
+            id_cliente = cliente.insertId;
+        } else id_cliente = cliente[0].id_cliente;
+
+        // Obtener id_vehiculo con la matricula seleccionada
+        sql = `SELECT id_vehiculo FROM vehiculos WHERE matricula = ?`;
+        params = [vehiculo];
+
+        const vehic = await query(sql, params);
+
+        if (vehic === 0) {
+            response.status(404).json({ mensaje: "No se ha podido encontrar el vehiculo" });
         }
 
-        //Se coge el id_usuario
-        sql = `SELECT * FROM usuarios WHERE correo = ?`;
-        params = [correo];
-        let usuario = await query(sql, params);
-        if(usuario.length == 0){
-            return response.status(401).json({ mensaje: "Correo no existente" });
-        }
-
-        //Se coge el id_vehiculo
-        sql = `SELECT * FROM vehiculos WHERE matricula = ?`;
-        params = [tipo];
-        let vehic = await query(sql, params);
-        if(vehic.length == 0){
-            return response.status(401).json({ mensaje: "Vehiculo no existente" });
-        }
+        const id_vehiculo = vehic[0].id_vehiculo;
+        const id_usuario = request.session.usuario.id_usuario; // Obtenido de la sesion guardada al iniciar sesión
 
         sql = `INSERT INTO reservas
-        (id_usuario, id_vehiculo, fecha_inicio, fecha_fin, estado, activo)
-        VALUES (?, ?, ?, ?, 'activa', true)`;
-        params = [usuario[0].id, vehic[0].id, fechaIni, fechaFin]
+        (id_usuario, id_vehiculo, id_cliente, fecha_inicio, fecha_fin, estado, activo)
+        VALUES (?, ?, ?, ?, ?, 'disponible', true)`;
+        params = [id_usuario, id_vehiculo, id_cliente, fechaIni, fechaFin];
+
         const resultado = await query(sql, params);
 
-        response.status(201).json({ mensaje: "Reserva creada correctamente" });
+        response.status(201).json({ mensaje: "Reserva creada", id: resultado.insertId });
 
     } catch (err) {
         console.error(err);
@@ -89,8 +113,8 @@ async function crearReserva(request, response) {
 // EDITAR RESERVAS
 async function actualizarReserva(request, response) {
     try {
-        const {estado, id_reserva} = request.params;
-        
+        const { estado, id_reserva } = request.params;
+
         const sql = `
         UPDATE reservas
         SET estado = ?
@@ -99,7 +123,7 @@ async function actualizarReserva(request, response) {
         const params = [estado, id_reserva];
         await query(sql, params);
 
-        return response.status(201).json({ mensaje: "Estado actualizado correctamente"});
+        return response.status(201).json({ mensaje: "Estado actualizado correctamente" });
     } catch (err) {
         console.error("Error al actualizar el estado:", err.message);
         return response.status(500).json({ error: "Error al actualizar el estado" });
@@ -111,7 +135,7 @@ async function obtenerReservasPorUsuario(request, response) {
     try {
         console.log("Acceso al controladorAPI de reservas por usuario");
         const sql = `SELECT * FROM reservas WHERE id_usuario = ?`;
-        const {id_usuario} = request.params;
+        const { id_usuario } = request.params;
         let reservas = await query(sql, [id_usuario]);
         response.json(reservas);
     } catch (err) {
