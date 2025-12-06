080de8d4d05a70e74f966b99c426935c3ac78e21
Leonardo <leonardo.ulffe.2003@gmail.com>
2025-12-06 01:02:56 +0100

    arreglando el parpadeo de navegar entre paginas cuando esta activo el modo oscuro

--- lab6-Express/routes/accesibilidad.js
+++ lab6-Express/routes/accesibilidad.js
@@ -1,22 +1,12 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
+const usuariosController = require("../controllers/usuariosController");
 
 // Obtener preferencias de sesión
-router.get("/obtener-preferencias", function (request, response) {
-    response.json(request.session.preferencias || {})
-});
+router.get("/obtener-preferencias", usuariosController.obtenerPreferencias);
 
 // Guardar preferencias
-router.post("/guardar-preferencias", function (request, response) {
-    const {clave, valor} = request.body;
-    if(!request.session.preferencias) {
-        request.session.preferencias = {};
-    }
-    request.session.preferencias[clave] = valor;
-    response.json({ok: true});
-})
-
-
+router.post("/guardar-preferencias", usuariosController.actualizarPreferencias);
 
 module.exports = router;
